f819729accceed29afd06bf63c9e94fd8193e0e9
Kasper Larsen <90896764+KasperWLarsen@users.noreply.github.com>
2026-05-01 11:32:00 +0200

    Add files via upload

---
+++ hj-lp-mig-med-at-lave/config.js
@@ -0,0 +1,54 @@
+window.CATERING_ADDIN_CONFIG = {
+  recipientEmail: "kwl@jual.dk",
+  companyName: "Intern frokost",
+  allowedPlaces: {
+    canteen: {
+      label: "Kantinen på Industrivej",
+      aliases: [
+        "kantine",
+        "kantinen",
+        "kantinen pa industrivej",
+        "kantinen på industrivej",
+        "industrivej kantine",
+        "canteen"
+      ]
+    },
+    savvaerksvej: {
+      label: "Kantinen på Savværksvej",
+      aliases: [
+        "kantinen pa savvaerksvej",
+        "kantinen på savværksvej",
+        "savvaerksvej",
+        "savværksvej",
+        "savvaerksvej kantine",
+        "savværksvej kantine"
+      ]
+    },
+    gammelgaardsvej: {
+      label: "Kantinen på Gammelgårdsvej",
+      aliases: [
+        "kantinen pa gammelgaardsvej",
+        "kantinen på gammelgårdsvej",
+        "gammelgaardsvej",
+        "gammelgårdsvej",
+        "gammelgardsvej",
+        "gammelgaardsvej kantine",
+        "gammelgårdsvej kantine"
+      ]
+    },
+    industrivej8: {
+      label: "Boardroom",
+      address: "Industrivej 8",
+      canteenLabel: "Kantinen på Industrivej",
+      aliases: [
+        "boardroom",
+        "industrivej 8",
+        "industrivej nr 8",
+        "industrivej nr. 8",
+        "industrivej nummer 8",
+        "nr 8",
+        "nr. 8"
+      ]
+    }
+  }
+};
